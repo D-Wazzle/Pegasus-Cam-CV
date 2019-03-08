33dd118dc5c7e914fafd1d48fa50ff7998c13e22
Dylan Wallace <wallad3@unlv.nevada.edu>
2019-03-08 04:23:47 -0800

    Added comments

--- arucoStream.py
+++ arucoStream.py
@@ -5,19 +5,52 @@ import aruco
 
 if __name__ == '__main__':
 
+	# get camera parameters
 	camparam = aruco.getParameters()
-	camparam.readFromXMLFile(dfk
+	camparam.readFromXMLFile(os.path.join(os.path.dirname(__file__), "dfk72_6mm_param2.yml"))
 
+        # create detector and get parameters
+        detector = aruco.MarkerDetector()
+        params = detector.getParameters()
+
+        # print detector parameters
+        print("detector params:")
+        for val in dir(params):
+        if not val.startswith("__"):
+            print("\t{} : {}".format(val, params.__getattribute__(val)))
+
+	# import Pegasus Cam stream
 	cap = cv2.VideoCapture("http://192.168.50.22:3000/html/cam_pic_new.php?time=9999999999999999999999999&pDelay=40000")
 
+	# read a frame from the stream
 	ret, frame = cap.read()
 	
+	# throw error if stream is bad
 	if not ret:
 		print("Can't open video!")
 		sys.exit(-1)
 
+	# while the stream is good
 	while ret:
+		# get markers in the frame
 		markers = detector.detect(frame)
 		
+		# Print marker coords
 		for marker in markers:
-			print("Marker: {:d}".format(marker.id))	
+			print("Marker: {:d}".format(marker.id))
+			for i, point in enumerate(marker):
+                		print("\t{:d} {}".format(i, str(point)))
+            		marker.draw(frame, np.array([255, 255, 255]), 2)
+
+            		# calculate marker extrinsics for marker size of 3.5cm
+            		marker.calculateExtrinsics(0.035, camparam)
+
+            		# print("Marker extrinsics:\n{}\n{}".format(marker.Tvec, marker.Rvec))
+            		print("detected ids: {}".format(", ".join(str(m.id) for m in markers)))
+
+        	# show frame
+        	cv2.imshow("frame", frame)
+        	cv2.waitKey(100)
+        
+        	# read next frame
+        	ret, frame = cap.read()
--- faceDetectStream.py
+++ faceDetectStream.py
@@ -2,43 +2,51 @@ import numpy as np
 import cv2
 from datetime import datetime
 
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
-
+# import Haar cascades
 face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml')
 
+# import Pegasus Cam stream
 cap = cv2.VideoCapture("http://192.168.50.22:3000/html/cam_pic_new.php?time=9999999999999&pDelay=40000")
 
+# while the stream is good
 while(cap.isOpened()):
+
+	# get current time
 	time_1 = datetime.now()
 	print(time_1)
 
+	# get current frame
 	ret, frame = cap.read()
 
+	# convert ot grayscale
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+	# detect faces in the frame
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	# display bounding boxes for face features
 	for(x, y, w, h) in faces:
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), -1)
 		roi_gray = gray[y:y+h, x:x+w]
 		roi_color = frame[y:y+h, x:x+w]
 		eyes = eye_cascade.detectMultiScale(roi_gray)
+		# dispay bounding boxes for eyes
 		for(ex, ey, ew, eh) in eyes:
 			cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
 	cv2.imshow('frame', frame)
 
-	#out.write(frame)
 
+	# get elapsed time
 	time_2 = datetime.now()
 	dt = time_2 - time_1
 	print(dt.seconds*1000 + dt.microseconds/1000)
 
+	# wait for ESC key to exit
 	k = cv2.waitKey(10)
-	if k == 27:         # wait for ESC key to exit
+	if k == 27:
     		cv2.destroyAllWindows()
 
 cap.release()
-#out.release()
 cv2.destroyAllWindows()
